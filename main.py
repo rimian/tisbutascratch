aa2fb8e31621438eaab972bad259dbc3c32b0d71
Rimian Perkins <hello@rimian.com.au>
2024-02-29 20:38:51 +1100

    add two commands

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from enum import Enum
 import typer
 
+app = typer.Typer()
+
+
 class ChangelogType(str, Enum):
     internal = "internal"
     api = "api"
     front_office = "office"
 
 
-def main(type: ChangelogType = ChangelogType.internal):
+@app.command()
+def start(type: ChangelogType = ChangelogType.internal):
     ticket_number = typer.prompt("What is the ticket number?")
     description = typer.prompt("What is the ticket description?")
-
     print(f"{ticket_number} of type: {type.value}")
-
     f = open(f"{type.value}/{ticket_number}-{description.lower().replace(' ', '-')}.md", "x")
     f.write(f"[{ticket_number}] - {description}")
 
 
+@app.command()
+def other():
+    pass
+
+
 if __name__ == "__main__":
-    typer.run(main)
+    app()
